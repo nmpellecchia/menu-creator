804a1d875806cd6e08ed69f0769b8a68b3c41ec7
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2022-02-21 18:05:54 -0300

    updated Dishcard Component

--- src/components/DishCard.jsx
+++ src/components/DishCard.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 
-function DishCard(props) {
+function DishCard({ dish, btn, onClick }) {
+  const handleClick = () => {
+    onClick(dish);
+  };
+
   return (
     <li className="w-11/12 md:w-2/5  m-6">
-      <div className="shadow-2xl rounded-2xl h-60 p-3">
+      <div className="shadow-2xl rounded-2xl p-3">
         <img
-          src={props.img || '#'}
-          alt={props.name}
+          src={dish.img || '#'}
+          alt={dish.name}
           className="w-7/12 float-right bg-red-500"
         />
-        <h3 className="font-semibold uppercase text-lg">{props.name}</h3>
-        <p className="font-semibold traking-wide">{props.price}</p>
+        <h3 className="font-semibold uppercase text-lg">{dish.title}</h3>
+        <p className="font-semibold traking-wide">${dish.price}</p>
         <p className="traking-widest font-bold text-lime-600 my-2">
-          {props.healthScore + '🔥'}
+          {dish.healthScore + '🔥'}
         </p>
-        <p>{props.time}</p>
+        <p>{dish.prepTime}mins</p>
 
         <div className="flex justify-between pt-6 clear-both">
           <button className="rounded-full bg-lime-700 py-2 px-4 text-white hover:bg-emerald-700">
             Details
           </button>
-          <button className="rounded-full bg-lime-700 py-2 px-4 text-white hover:bg-emerald-700">
-            Delete
+          <button
+            className="rounded-full bg-lime-700 py-2 px-4 text-white hover:bg-emerald-700"
+            onClick={handleClick}
+          >
+            {btn}
           </button>
         </div>
       </div>
