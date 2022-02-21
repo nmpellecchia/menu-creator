d1c3467e26a94dcfe2932916d34ce5d7b13243ef
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2022-02-21 18:09:37 -0300

    Searcher now gets and stores data
    - Dinamic card render based on the data from the API
    - get the dishes the user searched and display them
    - items can be stored after evaluation

--- src/utilities/storage/storage.js
+++ src/utilities/storage/storage.js
@@ -8,3 +8,73 @@ export function getFromLocalStorage(key) {
   const item = localStorage.getItem(key);
   return JSON.parse(item);
 }
+
+export function addNewItem(value) {
+  // This time I choose letting the function have the key. To avoid
+  // having to write it multiple times in the code
+  const key = 'selectedDishes';
+  //Retrieve from localstorage or create a new array if there isn't one
+  let items = JSON.parse(localStorage.getItem(key)) || [];
+
+  // If item already exists returns index, else -1
+  let i = items.findIndex(item => item.title == value.title);
+
+  if (items.length >= 4) {
+    return;
+  }
+  // Check if item is not already in the list
+  if (i !== -1) {
+    return;
+  }
+
+  if (!checkVeganDishes(items, value)) {
+    return;
+  }
+  items.push(value);
+
+  saveOnLocalStorage(key, items);
+}
+
+// This function should be tested with a library
+function checkVeganDishes(itemList, itemToAdd) {
+  let veganDishes = 0;
+
+  for (let i = 0; i <= itemList.length - 1; i++) {
+    if (itemList[i].vegan) {
+      veganDishes++;
+    }
+  }
+
+  /* if 2 dishes:
+                  0 vegans == ADD VEGAN
+                  1 vegan == do whatever
+                  2 vegans == ADD NOT VEGAN
+    if 3 dishes:
+                  1 vegan == ADD VEGAN
+                  2 vegan == ADD NOT VEGAN */
+
+  switch (veganDishes) {
+    case 2:
+      if (itemToAdd.vegan) {
+        alert('You have too many vegan dishes');
+        return false;
+      }
+      break;
+    case 1:
+      if (!itemToAdd.vegan && itemList.length == 3) {
+        alert('Add another vegan dish');
+        return false;
+      }
+      break;
+    case 0:
+      if (!itemToAdd.vegan && itemList.length == 2) {
+        alert('Add a vegan dish');
+        return false;
+      }
+      break;
+    default:
+      return true;
+  }
+  // If it passes the statements
+  return true;
+}
